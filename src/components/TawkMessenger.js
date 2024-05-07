22294979af45b937f827df1603d99bc88f57d619
Gusthiedo <guidogusthiabadi@yahoo.com>
2024-05-07 13:33:58 +0700

    first commit

---
+++ src/components/TawkMessenger.js
@@ -0,0 +1,15 @@
+import TawkMessengerReact from "@tawk.to/tawk-messenger-react";
+
+const TawkMessenger = () => {
+  return (
+    <div>
+      {/* Other content */}
+      <TawkMessengerReact
+        propertyId="6639ab8f9a809f19fb2e2448"
+        widgetId="1ht8kcar1"
+      />
+    </div>
+  );
+};
+
+export default TawkMessenger;
